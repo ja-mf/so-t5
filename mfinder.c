400c3478081c8316045e18674c0642f766beb632
adansio <adansio@gmail.com>
2012-07-16 02:39:07 -0400

    falta hacerlo recursivo para ingresar a subdirectorios

--- mfinder.c
+++ mfinder.c
@@ -4,17 +4,27 @@
 #include<dirent.h>
 #include<sys/stat.h>
 #include<unistd.h>
+#include<sys/types.h>
 
-int main ()
+int main (int argc, char **argv)
 {
+	//argc -> 2 , argv -> dueño y privilegios
 	DIR * directorio;
 	struct dirent * entry;
 	struct stat info;
-	int priv;		//pa probar por mientras los valores de entrada
+	int priv;	
 	int uid;
 
-	priv = 0700;
-	uid = 1000;
+	//UID de usuario efectivo en caso de no ingresar dueño.
+	if (argv[1] == NULL) uid = geteuid();
+	else uid = atoi(argv[1]);
+
+	//Privilegios asociados por umask en caso de no ingresarlos.
+	if (argv[2] == NULL) priv = umask(022); 
+	else priv = strtol(argv[2],NULL,8);
+	
+	//printf("arg1 = %d, arg2 = %i, arg3 = %o\n",argc,uid,priv);
+
 
 	//se abre directorio actual
 	directorio = opendir(".");
@@ -25,9 +35,11 @@ int main ()
 		exit(1);
 	}
 	
+	//se itera en directorio
 	while ( (entry = readdir(directorio)) != NULL)
 	{
 		if ( (strcmp(entry->d_name,".") ) != 0 && (strcmp(entry->d_name,"..") ) != 0 )
+
 			//se verfica estado del archivo
 			if ( stat(entry->d_name , &info) == -1 )
 			{
@@ -37,25 +49,21 @@ int main ()
 
 			else 
 			{
-				printf("nepe");
 				//printf("name: %s\t st_uid : %d\n",entry->d_name,info.st_uid);
 				//muestra archivo en el cual uid y los permisos sean los correspondientes
-				if (uid == info.st_uid)
+				if (uid == info.st_uid && priv == (info.st_mode & 000777))
 				{
-					printf("st_mode: %o\n",info.st_mode);
+					printf("st_mode:\t%o\n",info.st_mode);
 					printf("archivo:\t%s\n",entry->d_name);
-					if(info.st_mode & S_IRUSR) printf("\tpropietario tiene permiso r\n");
-					if(info.st_mode & S_IWUSR) printf("\tpropietario tiene permiso w\n");
-					if(info.st_mode & S_IXUSR) printf("\tpropietario tiene permiso x\n");
-					if(info.st_mode & S_IROTH) printf("\totro tiene permiso r\n");
-					if(info.st_mode & S_IWOTH) printf("\totro tiene permiso w\n");
-					if(info.st_mode & S_IXOTH) printf("\totro tiene permiso x\n");
-					int masked;
-					masked = (info.st_mode & 000777);
-					printf("\tcalza pollo: masked: %o, verify: %o\n", masked, (masked == priv)? 1 : 0 );
-//					if( access(entry->d_name,R_OK) == 0 ) printf("\tpermisos de lectura\n");
-//					if( access(entry->d_name,W_OK) == 0 ) printf("\tpermisos de escritura\n");
-//					if( access(entry->d_name,X_OK) == 0 ) printf("\tpermisos de ejecucion\n");
+					if(info.st_mode & S_IRUSR) printf("\tpropietario, permiso r\n");
+					if(info.st_mode & S_IWUSR) printf("\tpropietario, permiso w\n");
+					if(info.st_mode & S_IXUSR) printf("\tpropietario, permiso x\n");
+					if(info.st_mode & S_IRGRP) printf("\tgrupo, permiso r\n");
+					if(info.st_mode & S_IWGRP) printf("\tgrupo, permiso w\n");
+					if(info.st_mode & S_IXGRP) printf("\tgrupo, permiso x\n");
+					if(info.st_mode & S_IROTH) printf("\totro, permiso r\n");
+					if(info.st_mode & S_IWOTH) printf("\totro, permiso w\n");
+					if(info.st_mode & S_IXOTH) printf("\totro, permiso x\n");
 
 				}
 			}
