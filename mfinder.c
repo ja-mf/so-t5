48537082d3d088f9415efb60a70460a92d991cc1
adansio <adansio@gmail.com>
2012-07-15 22:45:49 -0400

    parte 5.3, en desarrollo

---
+++ mfinder.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<dirent.h>
+#include<sys/stat.h>
+#include<unistd.h>
+
+int main ()
+{
+	DIR * directorio;
+	struct dirent * entry;
+	struct stat info;
+	int priv;		//pa probar por mientras los valores de entrada
+	int uid;
+
+	priv = 0700;
+	uid = 0;
+
+	//se abre directorio actual
+	directorio = opendir(".");
+
+	if(directorio == NULL)
+	{
+		printf("No se puede acceder a directorio");
+		exit(1);
+	}
+	
+	while ( (entry = readdir(directorio)) != NULL)
+	{
+		if ( (strcmp(entry->d_name,".") ) != 0 && (strcmp(entry->d_name,"..") ) != 0 )
+			//se verfica estado del archivo
+			if ( stat(entry->d_name , &info) == -1 )
+			{
+				perror("stat");
+				exit(EXIT_FAILURE);
+			}
+
+			else 
+			{	
+				//printf("name: %s\t st_uid : %d\n",entry->d_name,info.st_uid);
+				//muestra archivo en el cual uid y los permisos sean los correspondientes
+				if (uid == info.st_uid)
+				{
+					printf("st_mode: %o\n",info.st_mode);
+					printf("archivo:\t%s\n",entry->d_name);
+					if(info.st_mode & S_IRUSR) printf("\tpropietario tiene permiso r\n");
+					if(info.st_mode & S_IWUSR) printf("\tpropietario tiene permiso w\n");
+					if(info.st_mode & S_IXUSR) printf("\tpropietario tiene permiso x\n");
+					if(info.st_mode & S_IROTH) printf("\totro tiene permiso r\n");
+					if(info.st_mode & S_IWOTH) printf("\totro tiene permiso w\n");
+					if(info.st_mode & S_IXOTH) printf("\totro tiene permiso x\n");
+//					if( access(entry->d_name,R_OK) == 0 ) printf("\tpermisos de lectura\n");
+//					if( access(entry->d_name,W_OK) == 0 ) printf("\tpermisos de escritura\n");
+//					if( access(entry->d_name,X_OK) == 0 ) printf("\tpermisos de ejecucion\n");
+
+				}
+			}
+	
+	}
+
+	closedir (directorio);
+
+	return 1;
+}
